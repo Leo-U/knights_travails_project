0cb755f5b5e33b0d033555d7de5bd2e7e0b18c29
Joel Howard <joelhoward2@gmail.com>
2023-03-03 19:25:51 -0500

    refactor code, rename/reorganize files

---
+++ lib/knight_travails.rb
@@ -0,0 +1,43 @@
+require_relative 'adjacency_list.rb'
+
+def knight_moves(start, target, queue = [start], dict = {}, adj = adjacency_list, path = [])
+  adj.each do
+    current = queue.shift
+    adj[current].each do |el|
+      enqueue_element(dict, el, queue)
+      add_dict_entry(dict, el, current)
+      return shortest_path(start, el, dict, path) if el == target
+    end
+  end
+end
+
+def shortest_path(start, el, dict, path)
+  path.unshift el
+  until el == start do
+    el = dict[el]
+    path.unshift el
+  end
+  puts "Your knight made it in #{path.count - 1} moves! Path taken:" 
+  path.each {|el| puts el}
+end
+
+def enqueue_element(dict, el, queue)
+  queue << el unless dict.include?(el)
+end
+
+def add_dict_entry(dict, el, current)
+  dict[el] = current unless dict.include?(el)
+end
+
+# outputs shortest path a chess knight can take to a target square
+
+
+knight_moves('a1', 'h8')
+# => Your knight made it in 6 moves! Path taken:
+# a1
+# b3
+# a5
+# b7
+# d6
+# f7
+# h8
