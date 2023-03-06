e16924f7381a8ce989e073e77c78871a4629935c
Joel Howard <joelhoward2@gmail.com>
2023-03-05 20:22:45 -0500

    add error handling for input

--- lib/main.rb
+++ lib/main.rb
@@ -4,11 +4,20 @@ knight = Knight.new
 
 end_condition = nil
 
+chess_notation = []
+('a'..'h').each do |letter|
+  (1..8).each do |number|
+    chess_notation << "#{letter}#{number}"
+  end
+end
+
 until end_condition == 'exit' || end_condition == 'n' || end_condition == 'no' do
   puts 'Enter starting square (use chess notation -- a1 or d4, etc)!'
   start = gets.chomp
+  raise "Quit horsin' around!" unless chess_notation.include?(start)
   puts 'Enter target square!'
   dest = gets.chomp
+  raise "Quit horsin' around!" unless chess_notation.include?(dest)
   knight.knight_moves(start, dest)
   puts '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
